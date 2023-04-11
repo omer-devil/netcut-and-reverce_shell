5051d460222524b64d157665146e60d2ad21ca7c
omer-devil <78538565+omer-devil@users.noreply.github.com>
2023-04-11 10:31:52 +0300

    Update Netcat.py

--- Netcat.py
+++ Netcat.py
@@ -1,23 +1,13 @@
 #!/usr/bin/python
-
 #-*-coding:utf8;-*-
-
 #qpy:console
-
 import subprocess 
-
 import random
-
 import socket
-
 import tqdm
-
 import time
-
 import sys
-
 import os
-
 
 BLUE, RED, WHITE, YELLOW, MAGENTA, GREEN, END = '\33[94m', '\033[91m', '\33[97m', '\33[93m', '\033[1;35m', '\033[1;32m', '\033[0m'
 
