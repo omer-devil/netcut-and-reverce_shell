93bf7416ac9b6e24818ee73069226067dc744176
omer-devil <78538565+omer-devil@users.noreply.github.com>
2023-04-17 18:54:21 +0300

    Update Netcat.py

--- Netcat.py
+++ Netcat.py
@@ -9,7 +9,6 @@ import time
 import sys
 import os
 
-# password of 000webhost = (#yT8w5JFz^*K6ELf)AVK)
 
 BLUE, RED, WHITE, YELLOW, MAGENTA, GREEN, END = '\33[94m', '\033[91m', '\33[97m', '\33[93m', '\033[1;35m', '\033[1;32m', '\033[0m'
 
